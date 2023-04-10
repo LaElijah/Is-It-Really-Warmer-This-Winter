786c158be4e31dcaa3c81dad8609e1202b7b643e
LaElijah <114874165+LaElijah@users.noreply.github.com>
2023-04-10 12:43:50 -0500

    forgot env port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-const port = 4000
+const port = process.env.PORT || 3000
 const weather = require('./routes/weather.js')
 require('dotenv').config();
 
